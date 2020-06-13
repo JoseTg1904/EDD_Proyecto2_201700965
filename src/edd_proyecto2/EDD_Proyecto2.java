8c3c2ebfb989ee0108e21d165bcda966a817c125
JoseTg1904 <josetenorguitarrista@gmail.com>
2020-06-12 20:52:09 -0600

    primer commit

---
+++ src/edd_proyecto2/EDD_Proyecto2.java
@@ -0,0 +1,16 @@
+package edd_proyecto2;
+
+/**
+ *
+ * @author chepe
+ */
+public class EDD_Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
